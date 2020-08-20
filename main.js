d1fcace2c59980a5c5f9153cafaf8d1f853f079b
LouisaLe <30714566+LouisaLe@users.noreply.github.com>
2020-08-20 15:49:51 +0700

    complete todo app

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // selector
 todoInput = document.querySelector('.todo__input');
-todoBtnAdd = document.querySelector('.todo__btn');
+todoBtnAdd = document.querySelector('.btn--add');
 todoListEle = document.querySelector('.todo__list');
 todoFilter = document.querySelector('.todo-filter');
 
 // todoItem = document.querySelector('.todo__item');
 
 // events
-
+document.addEventListener("DOMContentLoaded",getTodoListFromLocalStorage);
 todoBtnAdd.addEventListener("click", addTodo);
 todoListEle.addEventListener("click", actionOnItem);
 todoFilter.addEventListener("click", filterTodo);
@@ -28,7 +28,8 @@ function addTodo(e) {
 
     const todoDetail = document.createElement('div');
     todoDetail.classList.add('todo__input');
-    todoDetail.innerText = todoInput.value;
+    todoDetail.innerText = todoInput.value.trim();
+    saveLocalStorage(todoDetail.innerText);
     todoItemLi.appendChild(todoDetail);
 
     const todoActions = document.createElement('div');
@@ -46,12 +47,14 @@ function addTodo(e) {
 
 function actionOnItem(e) {
     let itemClicked = e.target;
+    console.log(e);
     if (itemClicked.classList[0] === 'btn--delete') {
         const todoItem = itemClicked.parentElement.parentElement;
         todoItem.classList.add('fall');
         todoItem.addEventListener('transitionend', () => {
             todoItem.remove();
         })
+        removeTodoItemFromLocalStorage(itemClicked.parentElement.previousElementSibling.innerText);
     }
 
     if (itemClicked.classList[0] === 'btn--done') {
@@ -95,3 +98,61 @@ function filterTodo(e) {
         }
     });
 }
+
+function saveLocalStorage(todo) {
+
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodoListFromLocalStorage() {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach((todo) => {
+        const todoItemLi = document.createElement('li');
+        todoItemLi.classList.add('todo__item');
+    
+        const todoDetail = document.createElement('div');
+        todoDetail.classList.add('todo__input');
+        todoDetail.innerText = todo;
+        todoItemLi.appendChild(todoDetail);
+    
+        const todoActions = document.createElement('div');
+        todoActions.classList.add('action-container');
+        todoActions.innerHTML = `
+                        <button class="btn--view"><i class="far fa-eye"></i></button>
+                        <button class="btn--done"><i class="fas fa-check"></i></button>
+                        <button class="btn--delete"><i class="fas fa-times"></i></button>`;
+        todoItemLi.appendChild(todoActions);
+        todoListEle.appendChild(todoItemLi);
+    });
+}
+
+function removeTodoItemFromLocalStorage(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    let index = todos.indexOf(todo);
+    if( index > -1) {
+        todos.splice(index,1);
+    }
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// TODO: set status for completed/uncompleted into localstorage
